f8ce851ff85dda1b148741a745c7af8c563670e1
rohittrimale <rohittrimalle@gmail.com>
2024-04-27 16:09:28 +0530

    We created contexxt and access with the help of context api

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcons";
+import CartContext from "../../store/cart-context";
+
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span className={classes.bump}>Your Cart</span>
-      <span className={classes.badge}>{3}</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
