b02352aadfc012d4ffda74f55dcc2320e4c67156
rohittrimale <rohittrimalle@gmail.com>
2024-04-27 13:41:13 +0530

    i have created the card with default value

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
+      <Cart />
       <Header />
-      <Meals />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "./Cart.module.css";
+import Model from "../UI/Modal";
+const Cart = (props) => {
+  const cartItems = (
+    <ul>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => {
+        return <li>{item.name}</li>;
+      })}
+    </ul>
+  );
+  return (
+    <Model >
+      {cartItems}
+      <div className={classes.total}>
+        <span>Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>order</button>
+      </div>
+    </Model>
+  );
+};
+
+export default Cart;
